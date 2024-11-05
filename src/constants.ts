1697f2a29ca9a4916abca1634048c18f32e8410b
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-05 20:06:51 +0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const TEXT_STRINGS: TTextStrings = {
   INVALID_URL: "❌ Invalid URL",
   FAILED_FETCH_PAGE_CONTENT: "❌ Failed to fetch page content",
   READ_TIME: `\n⏱️  Read Time: {readTime} minute(s).\n`,
-  EXIT_MSG: "Press any key to exit...",
+  EXIT_MSG: "Press any key(s) to exit...",
 };
 
 export const AVG_WORDS_PER_MINUTES: number = 200;
